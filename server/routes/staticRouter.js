c07c1494eb0bccb146c27b1686a40545ea30bff0
Aadity Dawkar <dawkarad2002@gmail.com>
2024-07-22 23:59:21 +0530

    css

--- server/routes/staticRouter.js
+++ server/routes/staticRouter.js
@@ -6,9 +6,15 @@ router.get("/", (req, res)=>{
     res.render("home")
 })
 
-router.get("/store", (req, res)=>{
-    res.render("store")
-})
+router.get('/store', async (req, res) => {
+    try {
+      const products = await Product.find(); // Fetch products from the database
+      res.render('store', { products }); // Pass products to the template
+    } catch (err) {
+      console.error('Error fetching products:', err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
 router.get("/faq", (req, res)=>{
     res.render("faq")
